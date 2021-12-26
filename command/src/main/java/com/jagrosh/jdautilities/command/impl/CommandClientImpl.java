28dc4f6cc92fa615372b5484eaec846a000650fb
Fontany--Legall Brandon <brandon@fontany-legall.xyz>
2021-12-26 17:27:01 +0100

    Fix of the default prefix @mention that was no longer responding to commands (#40)

--- command/src/main/java/com/jagrosh/jdautilities/command/impl/CommandClientImpl.java
+++ command/src/main/java/com/jagrosh/jdautilities/command/impl/CommandClientImpl.java
@@ -690,7 +690,10 @@ public class CommandClientImpl implements CommandClient, EventListener
         if(prefix.equals(DEFAULT_PREFIX) || (altprefix != null && altprefix.equals(DEFAULT_PREFIX))) {
             if(rawContent.startsWith("<@"+ event.getJDA().getSelfUser().getId()+">") ||
                     rawContent.startsWith("<@!"+ event.getJDA().getSelfUser().getId()+">")) {
-                final int prefixLength = rawContent.indexOf('>') + 1;
+                // Since we now use substring into makeMessageParts function and a indexOf here, we need to do a +1 to get the good substring
+                // On top of that we need to do another +1 because the default @mention prefix will always be followed by a space
+                // So we need to add 2 characters to get the correct substring
+                final int prefixLength = rawContent.indexOf('>') + 2;
                 return makeMessageParts(rawContent, prefixLength);
             }
         }
